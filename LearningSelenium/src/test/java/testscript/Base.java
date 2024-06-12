78980821683265fdc4df51d776db2bc13ce786b7
USER <USER@DESKTOP-K4CQR33>
2024-06-12 09:09:15 +0530

    New project with base class

---
+++ LearningSelenium/src/test/java/testscript/Base.java
@@ -0,0 +1,16 @@
+package testscript;
+
+public class Base {
+
+	public  void initializeBrowser() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Base base=new Base();
+		base.initializeBrowser();
+
+	}
+
+}
