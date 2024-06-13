c6d002b1f3d807e79a2db4636dee85c530992e38
USER <USER@DESKTOP-K4CQR33>
2024-06-13 09:00:20 +0530

    initial browser load methods

--- LearningSelenium/src/test/java/testscript/Base.java
+++ LearningSelenium/src/test/java/testscript/Base.java
@@ -1,15 +1,33 @@
 package testscript;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Base {
 
+	WebDriver driver;  							//always declare global since to be used across the project
 	public  void initializeBrowser() {
 		
+		driver=new ChromeDriver();				//this driver class might change for firefox, edge etc
+		driver.get("https://www.google.com/");  //to load url
+		driver.manage().window().maximize();    // to maximize the window
 	}
+	
+	
+	public void driverQuit() {
+		
+		driver.quit();
+	}
+	
+	
+	
 	
 	public static void main(String[] args) {
 		
 		Base base=new Base();
 		base.initializeBrowser();
+		base.driverQuit();
+		
 
 	}
 
---
+++ LearningSelenium/src/test/java/testscript/BrowserCommands.java
@@ -0,0 +1,18 @@
+package testscript;
+
+public class BrowserCommands extends Base{
+	
+public void browserCommands() {
+	
+		
+	}
+
+	public static void main(String[] args) {
+		
+		BrowserCommands browsercommands=new BrowserCommands();
+		browsercommands.initializeBrowser();
+		browsercommands.browserCommands();
+		browsercommands.driverQuit();
+	}
+
+}
